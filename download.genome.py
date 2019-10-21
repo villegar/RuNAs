6b2def1ba7791eb390abd19583f66a56d9412f77
Roberto Villegas-Diaz <roberto.villegasdiaz@sdstate.edu>
2019-10-21 14:53:38 -0500

    Minor changes

--- download.genome.py
+++ download.genome.py
@@ -2,11 +2,13 @@
 
 import json
 import multiprocessing
+from pathlib import Path
 import subprocess
 import sys
 
 def main(argv):
     CPUS = str(min([16, multiprocessing.cpu_count() - 1]))
+    GENOME_DIR = "GENOME"
     if(len(argv) != 2):
         print("error: 1 parameter expected, " + str(len(argv)-1) +" received")
         return -1
@@ -17,8 +19,14 @@ def main(argv):
         data = json.load(genome_data)
         for i in data:
             print("Downloading %s" % (i))
-            process = subprocess.Popen("aria2c -x " + CPUS + " -s " + CPUS + " " + data[i], shell=True, stdout=subprocess.PIPE)
-            output, error = process.communicate()
+            if(not Path(GENOME_DIR + "/" + Path(i).stem).exists()):
+                process = subprocess.Popen("aria2c -x " + CPUS + " -s " + CPUS + " " + data[i], shell=True, stdout=subprocess.PIPE)
+                output, error = process.communicate()
+            else:
+                print("File already downloaded")
+            if(not Path(GENOME_DIR + "/" + Path(i).stem).exists()):
+                process = subprocess.Popen("mkdir -p " + GENOME_DIR + " && mv " + i + " " + GENOME_DIR + " && yes n | gunzip " + GENOME_DIR + "/" + i + " && rm " + GENOME_DIR + "/" + i, shell=True, stdout=subprocess.PIPE)
+                output, error = process.communicate()
             print("Done")
     return 1
 
