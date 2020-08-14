e8a22850a3206bb71d493d4ddf2b17e5ef91f859
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-13 20:48:07 -0500

    New version

--- runas-logo.R
+++ runas-logo.R
@@ -5,4 +5,5 @@ hexSticker::sticker(subplot = "images/runas-logo.png",
                     # l_x = 1.0, l_y = 1.0, spotlight = FALSE, 
                     s_x = 1.0, s_y = .85, s_width = 1,
                     p_x = 1.0, p_y = 1.52, p_size = 6, p_color = "black",
+                    u_angle = 30, url = "https://github.com/villegar/RuNAs", u_size = 1.5,
                     filename = "images/logo.png")
