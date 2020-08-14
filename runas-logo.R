0681c5d0743d06bc5ad98814631ee6beceb0648e
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-13 20:39:24 -0500

    Script to create hex logo

---
+++ runas-logo.R
@@ -0,0 +1,8 @@
+hexSticker::sticker(subplot = "images/runas-logo.png", 
+                    package = "", 
+                    h_color = "#000000",  h_fill = "#FFFFFF",
+                    dpi = 800,
+                    # l_x = 1.0, l_y = 1.0, spotlight = FALSE, 
+                    s_x = 1.0, s_y = .85, s_width = 1,
+                    p_x = 1.0, p_y = 1.52, p_size = 6, p_color = "black",
+                    filename = "images/logo.png")
